3bf5a0c11b8a265e3976ddb5e74dd07413ee052b
Chandan-Kumar-Mahato <cmhandan@gmail.com>
2024-01-30 21:42:15 +0530

    p

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+const st = {textAlign:"center" , 
+color:"red"};
+function App() {
+
+  return (
+    <>
+      <h1>Library Management System</h1>
+      <p style={st}>Comming Soon...</p>
+    </>
+  )
+}
+
+export default App
